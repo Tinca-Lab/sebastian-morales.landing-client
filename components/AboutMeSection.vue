ee3fe28ee425a367145e09273a1685f9f6e35fc9
johan <johan352@gmail.com>
2023-04-21 12:57:05 -0500

    LSM-20 modifications and design updates in the my section component

---
+++ components/AboutMeSection.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="bg-white dark:bg-gray-900 ">
+        <div class="gap-16 py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 bg-gradient-to-r from-[#FFF7B2] to-[#FFD465]">
+            <div class="px-2 font-cabin pt-12 font-normal font-size-20 leading-150 text-gray-700 sm:text-lg dark:text-gray-400">
+                <h2 class="mb-4 text-2xl italic mx-auto leading-none text-[#FF8B1F] font-soulmaze md:text-5xl md:mx-0 md:font-soulmaze xs:text-center md:text-ce md:font-style-italic md:font-weight-400 xs:font-weight-400 md:line-height-30px md:letter-spacing-0em md:text-align-left">Soy Sebastián Morales</h2>
+                <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
+                <p >We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
+                <button class="flex flex-row justify-center mt-5 items-center gap-2 px-5 py-3 w-36 h-12 bg-[#1EB5E4] rounded-md shadow-md text-white mx-auto md:mx-0">
+                    <span class="text-lg font-medium">Leer más</span>
+                    <svg class="w-5 h-5 fill-current text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M12.293 6.20206C12.4805 6.01459 12.7348 5.90927 13 5.90927C13.2652 5.90927 13.5195 6.01459 13.707 6.20206L17.707 10.2021C17.8945 10.3896 17.9998 10.6439 17.9998 10.9091C17.9998 11.1742 17.8945 11.4285 17.707 11.6161L13.707 15.6161C13.5184 15.7982 13.2658 15.899 13.0036 15.8967C12.7414 15.8945 12.4906 15.7893 12.3052 15.6039C12.1198 15.4185 12.0146 15.1677 12.0123 14.9055C12.01 14.6433 12.1108 14.3907 12.293 14.2021L14.586 11.9091H3C2.73478 11.9091 2.48043 11.8037 2.29289 11.6162C2.10536 11.4286 2 11.1743 2 10.9091C2 10.6438 2.10536 10.3895 2.29289 10.202C2.48043 10.0144 2.73478 9.90906 3 9.90906H14.586L12.293 7.61606C12.1055 7.42853 12.0002 7.17422 12.0002 6.90906C12.0002 6.64389 12.1055 6.38959 12.293 6.20206V6.20206Z" fill="white"/>
+                    </svg>
+                </button>
+            </div>
+            <div class="grid grid-cols-2 gap-4 mt-8">
+                <img class="w-full rounded-lg" src="../assets/image/Imagen1.png" alt="office content 1">
+                <img class="mt-4 w-full lg:mt-10 rounded-lg" src="../assets/image/Imagen2.png" alt="office content 2">
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+    export default{
+        name: "AboutMeSection"
+    }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,100 +1,25 @@
 <template>
-   <!-- <section class="overflow-hidden relative bg-white dark:bg-gray-900">
-    <div class="gap-8 py-8 px-4 mx-auto max-w-screen-xl lg:py-16 xl:grid xl:grid-cols-12">
-        <div class="col-span-8">
-            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Discover new product and best possibilities</h1>
-            <p class="mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
-            <div class="gap-16 items-center sm:flex">
-                <div class="mb-8 text-gray-500 sm:mb-0 dark:text-gray-400">
-                    <svg class="mb-3 w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                    <h2 class="mb-3 text-xl font-semibold text-gray-900 dark:text-white">28 November 2021</h2>
-                    <p class="mb-4 font-light">Join us at FlowBite 2021 to understand what’s next as the global tech and startup ecosystem, rethinks the future of everything.</p>
-                    <a href="#" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                        Conference
-                        <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </a>
-                </div>
-                <div class="text-gray-500 dark:text-gray-400">
-                    <svg class="mb-3 w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z" clip-rule="evenodd"></path></svg>
-                    <h2 class="mb-3 text-xl font-semibold text-gray-900 dark:text-white">25+ top notch speakers</h2>
-                    <p class="mb-4 font-light">Here you will find keynote speakers, who all are able to talk about Recruiting. Click on the individual keynote speakers and read more about them and their keynotes.</p>
-                    <a href="#" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 inline-flex items-center">
-                        <svg class="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"></path></svg>
-                        View list
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="hidden absolute top-0 right-0 w-1/3 h-full xl:block">
-            <img class="object-cover w-full h-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/conference-speaker.jpg" alt="Conference speaker">
-        </div>
+    <div>
+        <About-Me-Section/>
     </div>
-</section> -->
-<section class="bg-white dark:bg-gray-900">
-  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-      <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blog</h2>
-          <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
-      </div>
-      <div class="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-          <article class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-              <a href="#">
-                  <img class="mb-5 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/blog/office-laptops.png" alt="office laptop working">
-              </a>
-              <span class="bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900">Article</span>
-              <h2 class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <a href="#">Our first office</a>
-              </h2>
-              <p class="mb-4 font-light text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation and some hard work, we moved to our new office.</p>
-              <div class="flex items-center space-x-4">
-                  <img class="w-10 h-10 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar">
-                  <div class="font-medium dark:text-white">
-                      <div>Jese Leos</div>
-                      <div class="text-sm font-normal text-gray-500 dark:text-gray-400">Aug 15, 2021 · 16 min read</div>
-                  </div>
-              </div>
-          </article>
-          <article class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-              <a href="#">
-                  <img class="mb-5 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/blog/google-hq.png" alt="Google HQ">
-              </a>
-              <span class="bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900">Article</span>
-              <h2 class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <a href="#">We partnered up with Google</a>
-              </h2>
-              <p class="mb-4 font-light text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation and some hard work, we moved to our new office.</p>
-              <div class="flex items-center space-x-4">
-                  <img class="w-10 h-10 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png" alt="Roberta Casas avatar">
-                  <div class="font-medium dark:text-white">
-                      <div>Roberta Casas</div>
-                      <div class="text-sm font-normal text-gray-500 dark:text-gray-400">Aug 15, 2021 · 16 min read</div>
-                  </div>
-              </div>
-          </article>
-          <article class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-              <a href="#">
-                  <img class="mb-5 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/blog/office-laptops-2.png" alt="office laptops">
-              </a>
-              <span class="bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900">Article</span>
-              <h2 class="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <a href="#">Our first project with React</a>
-              </h2>
-              <p class="mb-4 font-light text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation and some hard work, we moved to our new office.</p>
-              <div class="flex items-center space-x-4">
-                  <img class="w-10 h-10 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png" alt="Sofia McGuire avatar">
-                  <div class="font-medium dark:text-white">
-                      <div>Sofia McGuire</div>
-                      <div class="text-sm font-normal text-gray-500 dark:text-gray-400">Aug 15, 2021 · 16 min read</div>
-                  </div>
-              </div>
-          </article>
-      </div>
-  </div>
-</section>
 </template>
 
 <script>
+import AboutMeSection from '../components/AboutMeSection.vue';
 export default {
-  name: 'IndexPage',
+    name: "IndexPage",
+    components: [AboutMeSection],
 }
 </script>
+
+<style>
+@font-face {
+    font-family: "Soulmaze";
+    src: url("../assets/fonts/MADESoulmaze.otf");
+    }	
+
+    h2{
+        font-family: Soulmaze;
+        font-weight: lighter;
+    }
+</style>
