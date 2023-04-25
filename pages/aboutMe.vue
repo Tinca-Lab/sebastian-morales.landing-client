1af3cc4fab781e771217c72fc486224c94b10571
Bryan Vanegas <brayanxdvanegas@gmail.com>
2023-04-25 09:50:10 -0500

    'CreandoTimeLine'

--- pages/aboutMe.vue
+++ pages/aboutMe.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
-    <h1>About Me</h1>
+  <div class="mt-28">
+    <TimeLine />
   </div>
 </template>
 
 <script>
+import TimeLine from '../components/TimeLine/TimeLineComponent.vue'
 export default {
   name: "AboutMePage",
+  components: { TimeLine},
   layout : 'DefaultLayout',
 }
 
