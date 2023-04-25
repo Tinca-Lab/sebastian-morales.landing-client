80c860a1e59470035e5447795fad615b66bd89c2
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2023-04-25 14:14:41 -0500

    Fixing named components

--- components/Boton.vue
+++ components/Boton.vue
@@ -1,22 +1,22 @@
 <template>
-    <button >
-      <span class="text-lg font-medium">{{title}}</span>
-      <Arrow-Icon />
-    </button>
+  <button>
+    <span class="text-lg font-medium">{{ title }}</span>
+    <Arrow-Icon />
+  </button>
 </template>
 
 <script>
-    import ArrowIcon from './Icons/ArrowIcon.vue';
-    export default{
-      name: "BotonContact",
-      components: {
-        ArrowIcon
-      },
-      props: {
-        title: {
-          type: String,
-          default: ""
-        }
-      }
+import ArrowIcon from '@/components/icons/ArrowIcon.vue';
+export default {
+  name: "BotonContact",
+  components: {
+    ArrowIcon
+  },
+  props: {
+    title: {
+      type: String,
+      default: ""
     }
+  }
+}
 </script>
