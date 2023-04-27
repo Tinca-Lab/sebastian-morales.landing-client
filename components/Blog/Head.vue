d91857455f9377e59af81209479d2b9afa2ce449
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2023-04-27 16:22:19 -0500

    HomeAlpha

--- components/Blog/Head.vue
+++ components/Blog/Head.vue
@@ -1,39 +1,30 @@
 <template>
     <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16 ">
-        <h2 
-        class="mb-4 text-5xl tracking-tight font-extrabold text-[#003F52] dark:text-white"
-        :class="{'font-Inter':isMobile}">
-        {{ title }}
+        <h2 class="mb-4 text-5xl tracking-tight font-extrabold text-[#003F52] dark:text-white"
+            :class="{ 'font-Inter': isMobile }">
+            {{ title }}
         </h2>
-        <p
-        v-if="description.length>0"        
-        class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
+        <p v-if="description.length > 0" class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
             {{ description }}
-                <br>
-            {{ welcome }}
         </p>
     </div>
 </template>
 
 <script>
-export default{
-    name:"HeaderSection",
-    props:{
-        title:{
-            type:String,
-            default:""
+export default {
+    name: "HeaderSection",
+    props: {
+        title: {
+            type: String,
+            default: ""
         },
-        description:{
-            type:String,
-            default:""
-        },
-        welcome:{
-            type:String,
-            default:""
+        description: {
+            type: String,
+            default: ""
         },
     },
-    data(){
-        return{
+    data() {
+        return {
             isMobile: false,
         }
     },
