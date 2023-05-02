48cabd25c809454b754523495b505068bb009c7b
Bryan Vanegas <brayanxdvanegas@gmail.com>
2023-05-02 12:04:00 -0500

    Arreglando tamaño

--- components/TimeLine/TimeLineComponent.vue
+++ components/TimeLine/TimeLineComponent.vue
@@ -31,7 +31,7 @@
             </li>
         </ol>
 
-        <ol class="relative border-l border-gray-200 xs:w-[75%] md:w-[50%]">
+        <ol class="relative border-l border-gray-200 xs:w-full md:w-[50%]">
             <li class="mb-10 ml-4">
                 <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                 <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time>
