be3daa8298f85ef05d3aead1c7154871add0d39c
Bryan Vanegas <brayanxdvanegas@gmail.com>
2023-05-02 09:29:48 -0500

    Modificando el TimeLine"

--- components/TimeLine/TimeLineComponent.vue
+++ components/TimeLine/TimeLineComponent.vue
@@ -1,7 +1,7 @@
 <template>
- <section class="text-center flex flex-col gap-0 items-center w-full 2xl:w-[1440px] m-auto">
-    <div class="bg-[#85E2FF] rounded-3xl w-[98%] 2xl:w-[1440px] z-20 my-3">
-      <div class="max-w-screen-xl flex items-left mx-auto px-28	py-10">
+  <section class="text-center flex flex-col gap-0 items-center w-full 2xl:w-[1440px] m-auto">
+    <div class="bg-[#85E2FF] rounded-3xl w-[98%] 2xl:w-[1440px] z-20 my-3 h-50">
+     <!--  <div class="max-w-screen-xl flex items-left mx-auto px-28	py-10">
 
         <ol class="relative border-l border-gray-200 w-[50%]">
             <li class="mb-10 ml-4">
@@ -59,15 +59,67 @@
             </li>
         </ol>
 
+      </div -->
+
+      <div class="h-30 overflow-y-auto" ref="container" @scroll="handleScroll">
+        <div v-for="(child, index) in visibleChildren" :key="index" class="h-60">
+          <li class="mb-6 ml-4 p-3">
+            <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white"></div>
+            <time class="text-sm font-normal leading-none text-gray-400">{{ child.time }}</time>
+            <h3 class="text-lg font-semibold text-gray-900">{{ child.h3 }}</h3>
+            <p class="my-4 text-base font-normal text-gray-500">{{ child.p }}</p>
+          </li>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
+<!-- <li class="mb-10 ml-4">
+  <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white"></div>
+  <time class="mb-1 text-sm font-normal leading-none text-gray-400">{{ time }}</time>
+  <h3 class="text-lg font-semibold text-gray-900">{{ h3 }}</h3>
+  <p class="mb-4 text-base font-normal text-gray-500">{{ p }}</p>
+</li> -->
+
+
 <script>
   export default {
     name: "TimeLineComponent",
     components: {},
-  }
-  // git checkout -b LSM-26-desarrollo-del-timeline-vista-acerca-de-mi
+    scrollToTop: true,
+    data() {
+    return {
+      children: [
+        { time: 'February 2021', h3: 'Application UI code in Tailwind CSS 1',p: 'Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages. 1', },
+        { time: 'February 2022', h3: 'Application UI code in Tailwind CSS 2',p: 'Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages. 2', },
+        { time: 'February 2023', h3: 'Application UI code in Tailwind CSS 3',p: 'Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages. 3', },
+        { time: 'February 2024', h3: 'Application UI code in Tailwind CSS 4',p: 'Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages. 4', },
+        { time: 'February 2025', h3: 'Application UI code in Tailwind CSS 5',p: 'Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages. 5', },
+        { time: 'February 2026', h3: 'Application UI code in Tailwind CSS 6',p: 'Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages. 6', },
+        { time: 'February 2027', h3: 'Application UI code in Tailwind CSS 7',p: 'Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages. 7', },
+        { time: 'February 2028', h3: 'Application UI code in Tailwind CSS 8',p: 'Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages. 8', },
+        { time: 'February 2029', h3: 'Application UI code in Tailwind CSS 9',p: 'Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages. 9', },
+        { time: 'February 2030', h3: 'Application UI code in Tailwind CSS 10',p: 'Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages. 10', },
+      ],
+      visibleChildren: [],
+      startIndex: 0,
+      endIndex: 1,
+      pageSize: 2,
+    }
+  },
+  mounted() {
+    this.visibleChildren = this.children.slice(this.startIndex, this.endIndex + 1)
+  },
+  methods: {
+    handleScroll() {
+      const container = this.$refs.container
+      if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
+        this.startIndex += this.pageSize
+        this.endIndex += this.pageSize
+        this.visibleChildren = this.children.slice(this.startIndex, this.endIndex + 1)
+      }
+    },
+  },
+}
 </script>
--- pages/about-me.vue
+++ pages/about-me.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="mt-28">
-    <!-- <TimeLine /> -->
-    <div class="w-1/2">
+    <TimeLine />
+    <!-- <div class="w-1/2">
       <a class="twitter-timeline" href="https://twitter.com/JS_Morales?ref_src=twsrc%5Etfw">Tweets by TwitterDev</a>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-// import TimeLine from '@/components/TimeLine/TimeLineComponent.vue'
+import TimeLine from '@/components/TimeLine/TimeLineComponent.vue'
 export default {
   name: "AboutMePage",
-  // components: { TimeLine},
+  components: { TimeLine},
   layout: 'DefaultLayout',
 }
 </script>
